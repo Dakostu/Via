1088b594e90c6d088fbc2e57f80fec37666f0b67
Dakostu <Dkostuj@gmx.de>
2020-02-23 22:33:11 +0100

    added ShapeKeys

---
+++ shapes/shapekeys.h
@@ -0,0 +1,15 @@
+#ifndef SHAPEKEYS_H
+#define SHAPEKEYS_H
+
+
+namespace Via::Shapes {
+
+constexpr char DIAMOND_KEY = 'd';
+constexpr char HEXAGON_KEY = 'h';
+constexpr char OCTAGON_KEY = 'o';
+constexpr char SQUARE_KEY = 's';
+constexpr char TRIANGLE_DOWN_KEY = 'v';
+constexpr char TRIANGLE_UP_KEY = 'u';
+
+}
+#endif // SHAPEKEYS_H
